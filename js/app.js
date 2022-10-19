cf9706962b703315a388ae4fd97e321a6ee5e88a
Jacob Cohen <79770350+jacob187@users.noreply.github.com>
2022-10-19 19:51:49 -0400

    project 2 reqs: jQuery pre game actions

--- js/app.js
+++ js/app.js
@@ -1,10 +1,51 @@
-let firstName;
+// let firstName;
+// let score;
 
 welcome();
 
+$(document).ready(function () {
+   $("#gamespace").prepend(
+      "<img src='./img/baseball.png' alt='Baseball' id='baseball' />"
+   );
+
+   $(".timer").show();
+   $("#start_button").css({
+      "font-size": "18px",
+      "font-weight": "bold",
+      "font-family": "Roboto-Medium",
+      "background-color": "antiquewhite",
+      color: "rgb(73, 49, 16)",
+      "border-radius": ".5em",
+      width: "80px",
+      height: "54px",
+   });
+
+   alert(`x: ${random_x_coordinate()} y: ${random_y_coordinate()}`);
+
+   $("#start_button").click(function () {
+      alert();
+   });
+
+   score();
+});
+
 function welcome() {
    firstName = prompt("What is your first name?");
-   document.getElementById(
-      "welcome"
-   ).innerHTML = `Are you ready to play the game ${firstName}?`;
+   $("#welcome").html(`Are you ready to play the game ${firstName}?`);
+}
+
+function random_x_coordinate() {
+   let length = $("#gamespace").width();
+   return Math.floor(Math.random() * length);
+}
+
+function random_y_coordinate() {
+   let width = $("#gamespace").width();
+   return Math.floor(Math.random() * width);
+}
+
+function score() {
+   let score = 0;
+   score += 1;
+   $("#score").html(`${score} hits`);
 }
