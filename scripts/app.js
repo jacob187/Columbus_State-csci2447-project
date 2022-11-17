5b269d677a5b610d42a47839ea72d9875c188222
Jacob Cohen <79770350+jacob187@users.noreply.github.com>
2022-11-16 21:58:57 -0500

    refactored to ES6 arrow funcitons, Project4: Timer,score when click img with custom cursor in the gamespace

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,10 @@
-let firstName;
+let firstName = "";
 let score = 0;
 let secondsRemaining = 30;
 welcome();
 
 $(document).ready(function () {
+   $("#gamespace").on("click", "img", incrementScore);
    $("#start_button").css({
       "font-size": "18px",
       "font-weight": "bold",
@@ -14,8 +15,9 @@ $(document).ready(function () {
       width: "80px",
       height: "54px",
    });
-
+   //TODO: clear the timer and start counting down when start button gets clicken during a game
    $("#start_button").click(function () {
+      $("#score").show();
       start();
    });
 });
@@ -25,39 +27,58 @@ function welcome() {
    $("#welcome").html(`Are you ready to play the game ${firstName}?`);
 }
 
-function start() {
-   incrementScore();
+//when using ES6 arrow function stardards the following error is logged when the
+//function is called at the top of the program
+
+// Uncaught ReferenceError: can't access lexical declaration 'welcome' before initialization
+
+// const welcome = () => {
+//    firstName = prompt("What is your first name?");
+//    $("#welcome").html(`Are you ready to play the game ${firstName}?`);
+// };
+
+const start = () => {
    timer();
-   addImage();
-   console.log(`x: ${randomXCoordinate()} y: ${randomYCoordinate()}`);
-}
+};
 
-function randomXCoordinate() {
-   let length = $("#gamespace").width();
-   return Math.floor(Math.random() * length);
-}
-
-function randomYCoordinate() {
-   let width = $("#gamespace").width();
-   return Math.floor(Math.random() * width);
-}
-
-function timer() {
-   secondsRemaining -= 1;
-   $("#timer").html(`${secondsRemaining} seconds left.`);
-}
-
-function incrementScore() {
-   score += 1;
-   if (score == 1) {
-      $("#score").text(`${score} hit`);
+const timer = () => {
+   if (secondsRemaining != -1) {
+      //count down 30 -- 0
+      $("#timer").html(`${secondsRemaining} seconds left.`);
+      secondsRemaining--; // will be -1 at the end
+      setTimeout("addImage()", 2000); // rethink how to call images outside of the timer function as it does not fit the single purpose of this funciton
+      setTimeout("timer()", 1000);
    } else {
-      $("#score").text(`${score} hits`);
+      clearTimeout(timer);
+      clearScore();
+      gameOver();
    }
-}
+};
 
-function addImage() {
+const clearScore = () => {
+   score = 0;
+};
+const incrementScore = () => {
+   score++;
+   $("#score span").html(score);
+};
+
+const addImage = () => {
    $("#gamespace").prepend(
       "<img src='./images/baseball.png' alt='Baseball' id='baseball' />"
    );
-}
+};
+
+const randomXCoordinate = () => {
+   let length = $("#gamespace").width();
+   return Math.floor(Math.random() * length);
+};
+
+const randomYCoordinate = () => {
+   let width = $("#gamespace").width();
+   return Math.floor(Math.random() * width);
+};
+
+const gameOver = () => {
+   alert("Game Over!");
+};
